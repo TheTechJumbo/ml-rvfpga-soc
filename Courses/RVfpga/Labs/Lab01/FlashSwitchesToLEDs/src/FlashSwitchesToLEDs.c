4603803e6c6c81129068a49417de3cc3e60f4ce4
TheTechJumbo <mohammedaymanshaikh@gmail.com>
2024-12-06 23:40:59 +0000

    Completed FlashSwitchesToLEDs task in Lab01, Section 4, Exercise 1

--- Courses/RVfpga/Labs/Lab01/FlashSwitchesToLEDs/src/FlashSwitchesToLEDs.c
+++ Courses/RVfpga/Labs/Lab01/FlashSwitchesToLEDs/src/FlashSwitchesToLEDs.c
@@ -1,4 +1,25 @@
+#define GPIO_LEDs 0x80001404
 #define GPIO_INOUT 0x80001408
-#define READ_GPIO(dir) (*(volatile unsigned *)dir)
 #define WRITE_GPIO(dir, value) { (*(volatile unsigned *)dir) = (value); }
 
+void delay(int n) {
+    volatile int i;
+    for (i = 0; i < n; i++) {
+        ;
+    }
+}
+
+int main(void) {
+    int on = 0xFFFF; // Start with all LEDs on
+    WRITE_GPIO(GPIO_INOUT, on); // Set GPIO direction
+
+    while (1) {
+        WRITE_GPIO(GPIO_LEDs, on); // Turn on LEDs
+        delay(5000000); // Add a noticeable delay
+        on = ~on; // Invert the state
+        WRITE_GPIO(GPIO_LEDs, on); // Write inverted state
+        delay(5000000); // Another noticeable delay
+    }
+
+    return 0; // kinda redundant icl
+}
